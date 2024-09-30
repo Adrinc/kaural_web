b7df63350db4ed15d861cfe9796507ed524b9cc0
Abraham <abraham.armando.dominguez.rodriguez@uabc.edu.mx>
2024-09-29 18:00:50 -0700

    Seccion boletos añadida

--- src/components/global/animations/riveComponent.jsx
+++ src/components/global/animations/riveComponent.jsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 //NOTA: fit puede ser: "cover", "contain", "fill", "fitWidth", "fitHeight"
 
 export const RiveElement = (props) => {
-  const { src, stateMachines, autoplay, artboard, fit, hastext=false } = props;
+  const { src, stateMachines, autoplay, artboard, fit, hastext=false, textValues = {} } = props;
   const { RiveComponent, rive } = useRive({
     src: src,
     stateMachines: stateMachines?? null,
@@ -19,60 +19,44 @@ export const RiveElement = (props) => {
 
 
 
-
-  const onRiveEventReceived = (riveEvent) => {
-    const eventData = riveEvent.data;
-    const eventProperties = eventData.properties;
-
-    if (eventData.type === RiveEventType.General) {
-      console.log("Event name", eventData.name);
-      // Added relevant metadata from the event
-      console.log("Rating", eventProperties.rating);
-      console.log("Message", eventProperties.message);
-
-      // Update lable
-      let message = eventProperties.message;
-      setLable(message);
-    } else if (eventData.type === RiveEventType.OpenUrl) {
-      console.log("Event name", eventData.name);
-      // window.location.href = eventData.url;
-    }
-  };
-
-
   useEffect(() => {
     if (rive) {
+      console.log("Rive event");
       rive.on(EventType.RiveEvent, onRiveEventReceived);
-/* 
-      const getstateMachines = rive.stateMachineNames.map(getstateMachines => getstateMachines);;
-      const artboardNames = rive.contents.artboards.map(artboard => artboard.name);
-      console.log(artboardNames);
-      console.log(getstateMachines); */
 
      if(hastext){
-      console.log("Tiene texto");
-      //imprime solo el arttboard que se esta usando
-      console.log(rive.contents.artboards[0].name);
- 
-/*      console.log("Rive text was initially: ", rive.getTextRunValue("hs3_titulo"));
-     rive.getTextRunValue("hs3_titulo"); */
-     rive.setTextRunValue("hs3_titulo", "El kaural");
-     rive.setTextRunValue("hs3_subtitulo", "Subtitulo muy largo");
-     rive.setTextRunValue("hs3_descripcion", "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
+   
+      for (const [key, value] of Object.entries(textValues)) {
+        rive.setTextRunValue(key, value);
+      }
 
- 
+
     }
     }
   }, [rive]);
 
 
+  const onRiveEventReceived = (riveEvent) => {
+    const eventData = riveEvent.data;
+    const eventProperties = eventData.properties;
+    if (eventData.type === RiveEventType.General) {
+      console.log(eventData.name);
+      // Added relevant metadata from the event
 
+
+      console.log(eventProperties.mousein);
+      
+    } else if (eventData.type === RiveEventType.OpenUrl) {
+      console.log("Event name", eventData.name);
+      // Handle OpenUrl event manually      window.location.href = data.url;
+    }
+  };
 
   return <RiveComponent />;
 };
 
 export default function Animation(props) {
-  const { src, stateMachines, autoplay, artboard, fit, hastext } = props;
+  const { src, stateMachines, autoplay, artboard, fit, hastext, textValues } = props;
   return (
     <RiveElement 
     style={{width: "100%", height: "100%"}}
@@ -82,6 +66,7 @@ export default function Animation(props) {
     artboard={artboard} 
     fit={fit}
     hastext={hastext}
+    textValues={textValues}
     />
   );
 }
